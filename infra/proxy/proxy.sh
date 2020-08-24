68ca2e03f765da3d16fe1cc1c9426260f4b94d8c
Tavian Barnes <tavianator@tavianator.com>
2020-08-24 12:17:23 -0400

    infra/proxy: Enable HSTS and OCSP stapling

--- infra/proxy/proxy.sh
+++ infra/proxy/proxy.sh
@@ -6,7 +6,10 @@ nginx -g "daemon off;" &
 
 (
     while true; do
-        certbot --nginx --agree-tos -n --email tavianator@tavianator.com -d tavianator.com -d www.tavianator.com -d tavian.dev -d www.tavian.dev
+        certbot --nginx \
+                --redirect --hsts --staple-ocsp --must-staple \
+                --agree-tos -n --email tavianator@tavianator.com \
+                -d tavianator.com -d www.tavianator.com -d tavian.dev -d www.tavian.dev
         sleep $((24*60*60))
     done
 ) &
