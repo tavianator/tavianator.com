517aeb56e92dd8ea847a5a3b53c08d1b92f8b2c6
Tavian Barnes <tavianator@tavianator.com>
2022-03-03 15:35:09 -0500

    Hack to disable left/right arrow key navigation

--- src/tavianator.js
+++ src/tavianator.js
@@ -12,4 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
             code.parentNode.replaceChild(span, code);
         }
     }
+
+    let next = document.querySelector(".nav-chapters.next");
+    if (next) {
+        next.remove();
+    }
+
+    let prev = document.querySelector(".nav-chapters.previous");
+    if (prev) {
+        prev.remove();
+    }
 });
